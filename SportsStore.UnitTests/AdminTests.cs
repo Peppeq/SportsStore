5324f6e1b3f0677dd88f31dc5b05358e44977765
Peter <pedreto@azet.sk>
2016-05-18 22:49:56 +0200

    p.288

--- SportsStore.UnitTests/AdminTests.cs
+++ SportsStore.UnitTests/AdminTests.cs
@@ -62,6 +62,27 @@ namespace SportsStore.UnitTests
             Assert.AreEqual(2, p2.ProductID);
             Assert.AreEqual(3, p3.ProductID);
         }
+
+        [TestMethod]
+        public void Cannot_Edit_Nonexistent_Product()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            });
+
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+
+            // Act
+            Product result = (Product)target.Edit(4).ViewData.Model;
+
+            // Assert
+            Assert.IsNull(result);
+        }
     }
 }
 
